9b6e153a5533bdf016610e7deb425e7b4c639165
Adnan <adnan.khan@binmile.com>
2025-10-13 16:27:35 +0530

    Add wp-content and wp-includes

--- wordpress/scripts/before_install.sh
+++ wordpress/scripts/before_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo "Stopping Apache before deployment..."
+echo "Stopping Apache..."
 sudo systemctl stop apache2
 
-echo "Removing existing WordPress folder..."
+echo "Cleaning WordPress folder..."
 sudo rm -rf /var/www/html/wordpress/*
+
+echo "Setting proper permissions..."
+sudo chown -R ubuntu:ubuntu /var/www/html/wordpress
+sudo chmod -R 755 /var/www/html/wordpress
